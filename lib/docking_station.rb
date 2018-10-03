485ef54e3d36ad645e4ec2a9bad7bb7d788ed901
jebax <joel.eliot.baxter@gmail.com>
2018-10-03 14:47:24 +0100

    refactored to fulfil SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,13 +8,24 @@ class DockingStation
   end
 
   def release_bike
-    raise "There are no bikes" if @docked_bikes.empty?
+    raise "There are no bikes" if empty?
     @docked_bikes.pop
   end
 
   def dock(bike)
-    raise "Dock full" if @docked_bikes.count >= 20
+    raise "Dock full" if full?
     @docked_bikes << bike
     "#{bike} successfully docked"
   end
+
+  private
+
+  def full?
+    @docked_bikes.count >= 20
+  end
+
+  def empty?
+    @docked_bikes.empty?
+  end
+
 end
